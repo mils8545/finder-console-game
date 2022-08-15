a2a991dc6b8baf6855468e3c614ea256aa091c26
Chris Stewart <mils8545@gmail.com>
2022-08-14 21:15:34 -0600

    Added grid display and movement

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        bool gameOver = false;
+        Screen screen = new Screen();
+        screen.Clear();
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+
+        while (!gameOver)
+        {
+
+        	if (Console.KeyAvailable)
+        	{
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        screen.yCursor--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        screen.yCursor++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        screen.xCursor--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        screen.xCursor++;
+                        break;
+                    case ConsoleKey.Escape:
+                        gameOver = true;
+                        break;
+                }
+            }
+            screen.Draw();
+        }
+        Console.CursorVisible = true;
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ Screen.cs
@@ -0,0 +1,44 @@
+class Screen 
+{
+    public int xCursor = 0;
+    public int yCursor = 0;
+    public int width = 10;
+    public int height = 10;
+    public char[,] screen = new char[20, 20];
+
+    public void Clear()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                screen[i, j] = '.';
+            }
+        }
+        Console.Clear();
+    }
+    public void Draw()
+    {
+        Console.SetCursorPosition(0,0);
+        Console.CursorVisible = false;
+        xCursor = Math.Clamp(xCursor, 0, width - 1);
+        yCursor = Math.Clamp(yCursor, 0, height - 1);
+        for (int i = 0; i < width; i++)
+        {
+            string lineString = "";
+            for (int j = 0; j < height; j++)
+            {
+                if (i == yCursor && j == xCursor)
+                {
+                    lineString += "X" + " ";
+                }
+                else
+                {
+                    lineString += screen[i, j] + " ";
+                }
+            }
+
+            Console.WriteLine(lineString);
+        }
+    }
+}
